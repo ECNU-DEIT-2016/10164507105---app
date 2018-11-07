7651d354ef1cd97ca43f96b7ee021fcd574df041
ShepherdLee519 <m18506068984@163.com>
2018-11-07 15:27:31 +0800

    第一次作业

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,8 @@
 import 'package:angular/angular.dart';
 import 'package:firstapp/app_component.template.dart' as ng;
-import 'package:firstapp/app_component2.template.dart' as ng2;
 //import 'package:firstapp/randnum_component.template.dart' as ng;
 
 void main() {
   runApp(ng.AppComponentNgFactory);
-  runApp(ng2.AppComponent2NgFactory);
   //runApp(ng.RandNumComponentNgFactory);
 }
